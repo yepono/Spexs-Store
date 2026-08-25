b6bfeedc4eb8c38c5cb815aa7e3d74a5b7933f56
Yepo <tonbarrz5@gamil.com>
2026-08-25 12:52:43 -0600

    fix: main

--- React/React/src/main.jsx
+++ React/React/src/main.jsx
@@ -13,20 +13,6 @@ import { CarritoProvider } from './context/CarritoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NotificacionProvider>
-      <CarritoProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/catalogo" replace />} />
-            <Route path="/catalogo" element={<Catalogo />} >
-              <Route path="juego/:id" element={<DetalleJuego />} />
-            </Route>
-            {/* NUEVA RUTA */}
-            <Route path="/recibo" element={<Recibo />} />
-          </Routes>
-        </BrowserRouter>
-      </CarritoProvider>
-    </NotificacionProvider>
     <AuthProvider>
       <NotificacionProvider>
         <CarritoProvider>
