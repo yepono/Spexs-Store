e0acf3b0eacc6bdbeb62abd44f831d066a79da92
Chris Flores <floreschris0906@gmail.com>
2026-08-25 12:59:48 -0600

    fix de merge

--- React/React/src/main.jsx
+++ React/React/src/main.jsx
@@ -4,29 +4,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import Catalogo from './pages/Catalogo.jsx'
 import DetalleJuego from './pages/DetalleJuego.jsx'
-<<<<<<< Updated upstream
-=======
 import Recibo from './pages/Recibo.jsx'
 import Biblioteca from './pages/Biblioteca.jsx'
->>>>>>> Stashed changes
 
 import { AuthProvider } from './context/AuthContext.jsx'
 import { NotificacionProvider } from './context/NotificacionContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< Updated upstream
-    <NotificacionProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/catalogo" replace />} />
-          <Route path="/catalogo" element={<Catalogo />} >
-            <Route path="juego/:id" element={<DetalleJuego />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </NotificacionProvider>
-=======
     <AuthProvider>
       <NotificacionProvider>
         <CarritoProvider>
@@ -43,6 +28,5 @@ createRoot(document.getElementById('root')).render(
         </CarritoProvider>
       </NotificacionProvider>
     </AuthProvider>
->>>>>>> Stashed changes
-  </StrictMode>,
+  </StrictMode>
 )
