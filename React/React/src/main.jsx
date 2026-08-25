4089232f3d7abb1c4cd66f08ff07aaec27467651
Chris Flores <floreschris0906@gmail.com>
2026-08-25 12:18:02 -0600

    Feat: autenticacion de usuarios

--- React/React/src/main.jsx
+++ React/React/src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import Catalogo from './pages/Catalogo.jsx'
 import DetalleJuego from './pages/DetalleJuego.jsx'
+<<<<<<< Updated upstream
+=======
+import Recibo from './pages/Recibo.jsx'
+import Biblioteca from './pages/Biblioteca.jsx'
+>>>>>>> Stashed changes
 
+import { AuthProvider } from './context/AuthContext.jsx'
 import { NotificacionProvider } from './context/NotificacionContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+<<<<<<< Updated upstream
     <NotificacionProvider>
       <BrowserRouter>
         <Routes>
@@ -19,5 +26,23 @@ createRoot(document.getElementById('root')).render(
         </Routes>
       </BrowserRouter>
     </NotificacionProvider>
+=======
+    <AuthProvider>
+      <NotificacionProvider>
+        <CarritoProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Navigate to="/catalogo" replace />} />
+              <Route path="/catalogo" element={<Catalogo />}>
+                <Route path="juego/:id" element={<DetalleJuego />} />
+              </Route>
+              <Route path="/recibo" element={<Recibo />} />
+              <Route path="/biblioteca" element={<Biblioteca />} />
+            </Routes>
+          </BrowserRouter>
+        </CarritoProvider>
+      </NotificacionProvider>
+    </AuthProvider>
+>>>>>>> Stashed changes
   </StrictMode>,
 )
